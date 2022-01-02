6ed6775d06878ec00fb40569ddcf8211377b81a8
Leonardo Martes <lmartes@wawand.co>
2022-01-02 01:49:22 -0500

    Delegate methods are implemented.

--- meta-weather-api/Controllers/ViewController.swift
+++ meta-weather-api/Controllers/ViewController.swift
@@ -1,8 +1,11 @@
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, LocationSearchDelegate {
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
+    
+    private var locationSearchManager: LocationSearchManager = LocationSearchManager()
+    private var locationsSearch: [LocationSearchResponse] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -13,29 +16,45 @@ class ViewController: UIViewController {
         searchBar.delegate = self
         tableView.delegate = self
         tableView.dataSource = self
+        locationSearchManager.delegate = self
+    }
+    
+    func didUpdateLocationSearch(data: [LocationSearchResponse]) {
+        locationsSearch = data
+        tableView.reloadData()
+    }
+    
+    func didFailWithError(error: Error) {
+        //TODO: Add Modal "Location search is not valid."
+        print("didFailWithError Error: \(error)")
     }
 
 }
 
 //MARK: - Search Bar Delegate
 extension ViewController: UISearchBarDelegate {
-    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        locationSearchManager.searchLocation(location: searchText)
+    }
 }
 
 //MARK: - Table View Delegate
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return locationsSearch.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let locationCell = tableView.dequeueReusableCell(withIdentifier: "locationCellIdentifier") else {
+        guard let locationCell = tableView.dequeueReusableCell(withIdentifier: "locationCellIdentifier") as? LocationTableViewCell else {
             return UITableViewCell()
         }
-        
+        locationCell.setupView(data: locationsSearch[indexPath.row])
         return locationCell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("row selected: ", indexPath.row)
+    }
     
 }
 
